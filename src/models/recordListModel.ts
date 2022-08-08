73c158bcc0cd31614b1f31ff202e4946516ac9e2
edward <2219080450@qq.com>
2022-08-08 23:15:01 +0800

    改造Notes 使其成为通用组件

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -4,10 +4,10 @@ const recordListModel = {
         return JSON.parse(JSON.stringify(data))
     },
     fetch() {
-        return JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordItem[];
+        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     },
     save(data: RecordItem[]) {
-        window.localStorage.setItem('localStorageKeyName', JSON.stringify(data));
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
     }
 
 };
