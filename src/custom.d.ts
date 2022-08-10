eb64c3b5f19b142bd955efa8893b8e71bedb0f55
edward <2219080450@qq.com>
2022-08-10 20:57:53 +0800

    使用全局 tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,20 @@ type RecordItem = {  //TS类型声明只写类型，JS类型声明写具体内�
     amount: number
     createdAt?: Date
 }
+
+type Tag = {
+    id: string;
+    name: string;
+}
+type TagListModel = {
+    data: Tag[]
+    fetch: () => Tag[]
+    create: (name: string) => 'success' | 'duplicated'
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+    remove: (id: string) => boolean;
+    save: () => void
+}
+
+interface Window {   //声明window属性
+    tagList: Tag[];
+}
