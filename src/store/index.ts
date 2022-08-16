70884a2e1083c1ad34fc1904c159d98a4d86e282
edward <2219080450@qq.com>
2022-08-16 10:39:50 +0800

    save

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,16 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
+import router from '@/router';
+import any = jasmine.any;
 
 Vue.use(Vuex);
 
 type RootState = {
     recordList: RecordItem[],
     tagList: Tag[],
-    currentTag?: Tag
+    currentTag?: Tag,
+    record: any
 }
 
 const store = new Vuex.Store({
@@ -16,10 +19,41 @@ const store = new Vuex.Store({
         recordList: [],
         tagList: [],
         currentTag: undefined
+
     } as RootState,
     mutations: {
-        setCurrentTag: function (state, id: string) {
+        setCurrentTag(state, id: string) {
             state.currentTag = state.tagList.filter(t => t.id === id)[0];
+        },
+        updateTag(state, payload: { id: string, name: string }) {
+            const {id, name} = payload;
+            const idList = state.tagList.map(item => item.id);
+            if (idList.indexOf(id) >= 0) {
+                const names = state.tagList.map(item => item.name);
+                if (names.indexOf(name) >= 0) {
+                    window.alert('标签名重复了');
+                } else {
+                    const tag = state.tagList.filter(item => item.id === id)[0];
+                    tag.name = name;
+                    store.commit('saveTags');
+                }
+            }
+        },
+        removeTag(state, id: string) {
+            let index = -1;
+            for (let i = 0; i < state.tagList.length; i++) {
+                if (state.tagList[i].id === id) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index >=0 ) {
+                state.tagList.splice(index, 1);
+                store.commit('saveTags');
+                router.back();
+            }else{
+                window.alert('删除失败')
+            }
         },
         fetchRecords(state) {
             state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
@@ -35,7 +69,7 @@ const store = new Vuex.Store({
                 JSON.stringify(state.recordList));
         },
         fetchTags(state) {
-            return state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+             state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
         },
         createTag(state, name: string) {
             const names = state.tagList.map(item => item.name);
