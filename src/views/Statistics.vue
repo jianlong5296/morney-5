f3ee1fe79c1275d26554025e24d81539c1e0eec0
edward <2219080450@qq.com>
2022-08-18 11:26:42 +0800

    在 Money.vue 里面使用 Tabs

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,13 +1,13 @@
 <template>
-        <Layout>
-            <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
-            <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-            <div>
-                type:{{type}}
-                <br/>
-                interval:{{interval}}
-            </div>
-        </Layout>
+    <Layout>
+        <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+        <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+        <div>
+            type:{{type}}
+            <br/>
+            interval:{{interval}}
+        </div>
+    </Layout>
 </template>
 
 
@@ -16,30 +16,27 @@
     import {Component} from 'vue-property-decorator';
     import Types from '@/components/Money/Types.vue';
     import Tabs from '@/components/Tabs.vue';
+    import recordTypeList from '@/constants/recordTypeList';
+    import intervalList from '@/constants/intervalList';
 
     @Component({
         components: {Tabs, Types},
     })
     export default class Statistics extends Vue {
         type = '-';
-        interval = 'day'
-        intervalList = [
-            {text:'按天',value:'day'},
-            {text:'按周',value:'week'},
-            {text:'按月',value:'month'},
-            ];
-        typeList = [
-            {text:'支出',value:'-'},
-            {text:'收入',value:'+'},
-        ]
+        interval = 'day';
+        intervalList = intervalList;
+        recordTypeList = recordTypeList;
     };
 </script>
 <style scoped lang="scss">
-     ::v-deep .type-tabs-item {
+    ::v-deep .type-tabs-item {
         background: white;
+
         &::after {
             display: none;
         }
+
         &.selected {
             background: #c4c4c4;
         }
