f7041018bd3ec92fbb6176a48160a00f2cfa4702
edward <2219080450@qq.com>
2022-08-18 11:41:19 +0800

    更新 CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
     <Layout>
-        <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+        <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
         <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
         <div>
             type:{{type}}
@@ -14,19 +14,18 @@
 <script lang="ts">
     import Vue from 'vue';
     import {Component} from 'vue-property-decorator';
-    import Types from '@/components/Money/Types.vue';
     import Tabs from '@/components/Tabs.vue';
     import recordTypeList from '@/constants/recordTypeList';
     import intervalList from '@/constants/intervalList';
 
     @Component({
-        components: {Tabs, Types},
+        components: {Tabs},
     })
     export default class Statistics extends Vue {
         type = '-';
         interval = 'day';
         intervalList = intervalList;
-        recordTypeList = recordTypeList;
+        typeList = recordTypeList;
     };
 </script>
 <style scoped lang="scss">
@@ -40,6 +39,9 @@
         &.selected {
             background: #c4c4c4;
         }
+    }
+    ::v-deep .interval-tabs-item{
+        height: 48px;
     }
 
 </style>
