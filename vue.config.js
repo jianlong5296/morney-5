ebcff2f1a7d761cfe74d2f7ded0caa0fcf09b26e
edward <2219080450@qq.com>
2022-08-22 12:21:15 +0800

    优化 pc 界面的样式

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/morney-5/'
+        : '/',
   lintOnSave: false,
   chainWebpack:config=>{
     const dir = path.resolve(__dirname,'src/assets/icons')
